1a6940031aed197b2f061de3d3a8833cad983d84
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-07 17:47:17 +0530

    Update approve_sql.py

--- JennieRobot/modules/sql/approve_sql.py
+++ JennieRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from TGNRobot.modules.sql import BASE, SESSION
+from JennieRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
