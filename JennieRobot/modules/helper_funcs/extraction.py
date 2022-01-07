b20029f90481f2545280ab6d13939a8a3300d927
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-07 17:41:17 +0530

    Update extraction.py

--- JennieRobot/modules/helper_funcs/extraction.py
+++ JennieRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from TGNRobot import LOGGER
-from TGNRobot.modules.users import get_user_id
+from JennieRobot import LOGGER
+from JennieRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
