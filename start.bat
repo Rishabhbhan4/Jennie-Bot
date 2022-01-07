4f22ddfdb04a3ea95dad823d28574bf3bfb99357
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-07 16:25:16 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE TGN Robot
+TITLE Jennie Bot
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m TGNRobot
+env\scripts\activate.bat && py -m Jennie-Bot
