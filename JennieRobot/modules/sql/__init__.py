01223a6fdce3bd692563f515c5fb858f3cd70a8d
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-07 17:44:46 +0530

    Update __init__.py

--- JennieRobot/modules/sql/__init__.py
+++ JennieRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from TGNRobot import DB_URI
+from JennieRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
