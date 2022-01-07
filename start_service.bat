8d578d2b50f2d05d3506c9fae30983833824acad
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-07 16:26:10 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Jennie-Bot
-net start Jennie-Bot
+net stop JennieRobot
+net start JennieRobot
