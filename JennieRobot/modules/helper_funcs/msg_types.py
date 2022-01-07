aa81622cc5a56261c79ff577662eb08a539602fa
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-07 17:43:50 +0530

    Update msg_types.py

--- JennieRobot/modules/helper_funcs/msg_types.py
+++ JennieRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from TGNRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from JennieRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
