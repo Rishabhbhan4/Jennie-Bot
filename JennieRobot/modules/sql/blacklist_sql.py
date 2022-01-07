d8b166f7e80866573e4550713119d836abdefe86
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-07 17:47:39 +0530

    Update blacklist_sql.py

--- JennieRobot/modules/sql/blacklist_sql.py
+++ JennieRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from TGNRobot.modules.sql import SESSION, BASE
+from JennieRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
