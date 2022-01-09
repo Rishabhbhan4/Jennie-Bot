30ddcda6fcad698229dd8617104351dd86769587
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-09 11:58:08 +0530

    Update errors.py

--- JennieRobot/pyrogramee/errors.py
+++ JennieRobot/pyrogramee/errors.py
@@ -1,7 +1,7 @@
 import sys
 import traceback
 from functools import wraps
-from TGNRobot import pbot, SUPPORT_CHAT
+from JennieRobot import pbot, SUPPORT_CHAT
 
 def split_limits(text):
     if len(text) < 2048:
