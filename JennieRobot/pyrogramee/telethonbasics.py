6380aa9c70c5e5ff603cdfdd169e8cfb885e27cf
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-09 11:58:36 +0530

    Update telethonbasics.py

--- JennieRobot/pyrogramee/telethonbasics.py
+++ JennieRobot/pyrogramee/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from TGNRobot.pyrogramee.pluginshelper import runcmd
+from JennieRobot.pyrogramee.pluginshelper import runcmd
 
 
 async def convert_to_image(event, borg):
