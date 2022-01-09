262cb90dd0ee55fa1aedd5ab5e5dad3b7be05492
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-09 11:51:54 +0530

    Update feds_sql.py

--- JennieRobot/modules/sql/feds_sql.py
+++ JennieRobot/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from TGNRobot import dispatcher
-from TGNRobot.modules.sql import BASE, SESSION
+from JennieRobot import dispatcher
+from JennieRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
