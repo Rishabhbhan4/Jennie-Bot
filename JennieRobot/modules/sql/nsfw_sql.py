9b9257a0ccc7e3860fed57f85c89919b3e5f9c5e
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-09 11:54:35 +0530

    Update nsfw_sql.py

--- JennieRobot/modules/sql/nsfw_sql.py
+++ JennieRobot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from TGNRobot.modules.sql import BASE, SESSION
+from JennieRobot.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @Timesisnotwaiting |
 #   |        Kang with Credits         |
