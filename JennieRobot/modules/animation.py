bc31650412bb21d739fc861974da056d8e3dc5ef
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-10 17:01:36 +0530

    Update animation.py

--- JennieRobot/modules/animation.py
+++ JennieRobot/modules/animation.py
@@ -4,9 +4,9 @@ from typing import List
 from telegram import Update
 from telegram.ext import run_async,CallbackContext
 
-from TGNRobot import dispatcher
-from TGNRobot.modules.disable import DisableAbleCommandHandler
-from TGNRobot.modules.helper_funcs.chat_status import user_admin
+from JennieRobot import dispatcher
+from JennieRobot.modules.disable import DisableAbleCommandHandler
+from JennieRobot.modules.helper_funcs.chat_status import user_admin
 
 #sleep how many times after each edit in 'love' 
 EDIT_SLEEP = 1
