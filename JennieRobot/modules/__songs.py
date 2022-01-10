e64cd4b67b189c6f515cdb841085c773d0689399
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-10 16:49:34 +0530

    Update __songs.py

--- JennieRobot/modules/__songs.py
+++ JennieRobot/modules/__songs.py
@@ -6,8 +6,8 @@ import requests
 import wget
 from pyrogram import filters
 
-from TGNRobot import pbot as zaid
-from TGNRobot.pyrogramee.dark import get_arg
+from JennieRobot import pbot as Jennie
+from JennieRobot.pyrogramee.dark import get_arg
 
 
 @zaid.on_message(filters.command("saavn"))
