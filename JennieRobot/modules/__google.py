00c251fd0ac4dea51c31623c57f8fc60796e37b7
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-10 16:38:38 +0530

    Update __google.py

--- JennieRobot/modules/__google.py
+++ JennieRobot/modules/__google.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib
-from TGNRobot import telethn as tbot
+from JennieRobot import telethn as tbot
 import glob
 import io
 import os
@@ -21,9 +21,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from TGNRobot import *
+from JennieRobot import *
 
-from TGNRobot.events import register
+from JennieRobot.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> @AwesomeSupport <==="
+        app_details += "\n\n===> @lisaSupportChat <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
