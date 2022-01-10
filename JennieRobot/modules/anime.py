0bf51aa77b6b862608675d2792f314bd536e3b2f
Rishabhbhan4 <96171226+Rishabhbhan4@users.noreply.github.com>
2022-01-10 17:02:47 +0530

    Update anime.py

--- JennieRobot/modules/anime.py
+++ JennieRobot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from TGNRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from TGNRobot.modules.disable import DisableAbleCommandHandler
+from JennieRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from JennieRobot.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
