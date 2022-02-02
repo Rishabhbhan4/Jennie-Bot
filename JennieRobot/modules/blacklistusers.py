05769e4dba07a4c77ebb306b9cfdf5d8b4d1faff
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-02-02 21:06:53 +0530

    Update blacklistusers.py

--- JennieRobot/modules/blacklistusers.py
+++ JennieRobot/modules/blacklistusers.py
@@ -1,7 +1,7 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import TGNRobot.modules.sql.blacklistusers_sql as sql
-from TGNRobot import (
+import JennieRobot.modules.sql.blacklistusers_sql as sql
+from JennieRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from TGNRobot import (
     WOLVES,
     dispatcher,
 )
-from TGNRobot.modules.helper_funcs.chat_status import dev_plus
-from TGNRobot.modules.helper_funcs.extraction import (
+from JennieRobot.modules.helper_funcs.chat_status import dev_plus
+from JennieRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from TGNRobot.modules.log_channel import gloggable
+from JennieRobot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
