54eeb7eaf676bd7d3df87bad3b4443add1648a36
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-02-04 08:56:23 +0530

    Update dev.py

--- JennieRobot/modules/dev.py
+++ JennieRobot/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import TGNRobot
+import JennieRobot
 
-from TGNRobot import dispatcher
-from TGNRobot.modules.helper_funcs.chat_status import dev_plus
+from JennieRobot import dispatcher
+from JennieRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
