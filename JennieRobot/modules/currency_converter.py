c91da953e8926ec0befd5e90e7d0998d1055f162
Rishabhbhan5 <90978799+Rishabhbhan5@users.noreply.github.com>
2022-02-04 08:52:47 +0530

    Update currency_converter.py

--- JennieRobot/modules/currency_converter.py
+++ JennieRobot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from TGNRobot import CASH_API_KEY, dispatcher
+from JennieRobot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
